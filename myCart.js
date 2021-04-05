366f3634a0b05d3d859cd55f42f49292ba946eca
Mohammad Altamimi <m98altamimi@gmail.com>
2021-04-05 16:59:43 +0300

    Jd

--- myCart.js
+++ myCart.js
@@ -148,7 +148,7 @@ let totalHeader=document.createElement('th');
 totalHeader.textContent='TOTAL';
 footerRow.appendChild(totalHeader);
 let newCell=document.createElement('td');
-newCell.textContent= total;
+newCell.textContent=`${total} Jd` ;
 footerRow.appendChild(newCell);
 
 tableFooter.setAttribute('id','tfoot');
@@ -268,7 +268,7 @@ function updateTotal(){
   totalHeader.textContent='TOTAL';
   footerRow.appendChild(totalHeader);
   newCell=document.createElement('td');
-  newCell.textContent= total;
+  newCell.textContent= `${total} Jd`;
   footerRow.appendChild(newCell);
 }
 
