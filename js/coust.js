e9e0a1e0ff4cac0dac6a2492b32b27cd00aa70f3
amr88nzzal <amr.nzzal@gmail.com>
2021-04-05 05:10:57 +0300

    updat js+css in custom..

--- js/coust.js
+++ js/coust.js
@@ -1,40 +1,39 @@
 'use strict';
-
+let cartNameEl =document.getElementById('cartName');
 let cateElX=document.getElementsByClassName('cate');
 for(let i=0;i<cateElX.length;i++){
   cateElX[i].addEventListener('click',showItems);}
 let giftItemsElX=document.getElementsByClassName('giftItems');
 function showItems(event){
-  let y;
-  let z=giftItemsElX;
-  // console.log(z);
-  for(let i=0;i<z.length;i++){
-    z[i].setAttribute('style',' display:none');
+  let btnCateg;
+  // let z=giftItemsElX;
+  // // console.log(z);
+  for(let i=0;i<giftItemsElX.length;i++){
+    giftItemsElX[i].setAttribute('style',' display:none');
   }
   switch (event.target.parentElement.id){
   case 'cate1':
-    y=document.getElementById('giftItems1');
-    y.style.display='inline-flex';
+    btnCateg=document.getElementById('giftItems1');
+    btnCateg.style.display='inline-flex';
     break;
   case 'cate2':
-    y=document.getElementById('giftItems2');
-    y.style.display='inline-flex';
+    btnCateg=document.getElementById('giftItems2');
+    btnCateg.style.display='inline-flex';
     break;
   case 'cate3':
-    y=document.getElementById('giftItems3');
-    y.style.display='inline-flex';
+    btnCateg=document.getElementById('giftItems3');
+    btnCateg.style.display='inline-flex';
     break;
   case 'cate4':
-    y=document.getElementById('giftItems4');
-    y.style.display='inline-flex';
+    btnCateg=document.getElementById('giftItems4');
+    btnCateg.style.display='inline-flex';
     break;
   }
 }
-let yy=0;
-let x=document.getElementsByClassName('giftItem');
-// console.log(x[0]);
-for(let i=0;i<x.length;i++){
-  x[i].addEventListener('submit',btn);}
+let btnAdd2Cart=document.getElementsByClassName('giftItem');
+
+for(let i=0;i<btnAdd2Cart.length;i++){
+  btnAdd2Cart[i].addEventListener('submit',btn);}
 function btn (event){
   event.preventDefault();
   // debugger;
@@ -43,18 +42,35 @@ function btn (event){
   let giftPrice =event.target.children[1].textContent;
   let giftQty = event.target.children[2].value;
   let btnAdd = event.target.children[3].value;
-
+  let added=0;
   if (btnAdd==='Add To Cart'){
-    let giftOrder =new Gifts;
-    giftOrder.giftName=giftName;
-    giftOrder.giftPrice=giftPrice;
-    giftOrder.giftQty=giftQty;
-    giftOrder.giftpath=giftpath;
-    console.log(`giftName ${giftName}\ngiftpath ${giftpath}\ngiftPrice ${giftPrice} \ngiftQty ${giftQty} \nbtnAdd ${btnAdd}`) ;
-    console.log(Gifts.all);
+    if(Gifts.all.length>0){
+      for(let i=0;i<Gifts.all.length;i++){
+        if(Gifts.all[i].giftName===giftName){
+          Gifts.all[i].giftQty=parseInt(Gifts.all[i].giftQty,10)+ parseInt(giftQty,10);
+          added=1;
+          break;}
+      }
+      if(added!==1){
+        let giftOrder =new Gifts;
+        giftOrder.giftName=giftName;
+        giftOrder.giftPrice=giftPrice;
+        giftOrder.giftQty=giftQty;
+        giftOrder.giftpath=giftpath;
+        console.log(Gifts.all,1000);}
+    }
+    else{
+      let giftOrder =new Gifts;
+      giftOrder.giftName=giftName;
+      giftOrder.giftPrice=giftPrice;
+      giftOrder.giftQty=giftQty;
+      giftOrder.giftpath=giftpath;
+      console.log(Gifts.all,1010);
+    }
     localStorage.setItem('gift2cart',JSON.stringify(Gifts.all));
-  }
-}
+    cartNameEl.textContent=`Cart (${Gifts.all.length})`;
+  }}
+
 
 const Gifts = function(giftName,giftpath,giftPrice,giftQty){
   this.giftName=giftName;
@@ -65,3 +81,7 @@ const Gifts = function(giftName,giftpath,giftPrice,giftQty){
   Gifts.all.push(this);
 };
 Gifts.all=[];
+
+
+//////////////////////////////
+
